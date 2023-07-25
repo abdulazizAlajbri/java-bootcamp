bc867333e13eeafb1d513b83240739534d9398aa
Abdulaziz Aljabri <abdulaziz.aljabri18@gmail.com>
2023-07-25 20:52:06 +0300

    HomeWork3

--- src/homework/three/QuestionSix.java
+++ src/homework/three/QuestionSix.java
@@ -9,20 +9,23 @@ public class QuestionSix {
     }
 
     public static void answer() {
-        System.out.println("## Prints the sum of the even and odd integers ##");
         Scanner input = new Scanner(System.in);
         int evenSum = 0;
         int oddSum = 0;
-        System.out.println("Enter the Number : ");
-        int number = input.nextInt();
-        while (number != 0) {
-            if (number % 2 == 0)
+        System.out.println("Please enter the Count of numbers you want to find the sum of");
+        int count = input.nextInt();
+        for (int index = 1; index <= count; index++) {
+            System.out.println("Enter the number : ");
+            int number = input.nextInt();
+            if (number % 2 == 0) {
                 evenSum += number;
-            else
+            } else {
                 oddSum += number;
+            }
         }
         System.out.println("The sum of even numbers is : " + evenSum);
         System.out.println("The sum of odd numbers is : " + oddSum);
+
     }
 
 }
